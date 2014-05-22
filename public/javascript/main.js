609dd2d64c8e4cabfb8d7c1f708790e79dca9a2b
Andrew Dotey Jr <adotey@stanford.edu>
2014-05-22 04:14:36 -0700

    Minor UI changes, added links to homepage

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -31,7 +31,6 @@ var current_user_is_owner = false; //flag that says whether the current user is 
 
   $(document).ready(function(){
     connect_to_chat_firebase();
-    connect_webcam();
   });
 
 
@@ -94,11 +93,12 @@ var current_user_is_owner = false; //flag that says whether the current user is 
        // alert('User is the owner.');
        document.getElementById("reactions").style.display="block";
        document.getElementById("friend_guide").style.display="none";
+       document.getElementById("footer").style.display="none";
       } else {
         //alert('User is NOT the owner');
+        connect_webcam();
         document.getElementById("reactions").style.display="none";
         document.getElementById("owner_guide").style.display="none";
-
       }
     });
 
